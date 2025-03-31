8f5e43dc6e782f04bf802ee15416da3f109e66f0
SergioCaMi <sergiocami84@gmail.com>
2025-03-31 21:54:53 +0200

    ejercicio DOM empezado

--- dom/app.js
+++ dom/app.js
@@ -1,1 +1,14 @@
 // ¿En tu páis de origen es normal dejar propina?
+//en mi país es voluntario, y eso ya es cada cual... xD (yo suelo dejar algo)
+
+
+const inputBillAmount = document.getElementById("bill-amount");
+const inputPercentageTip = document.getElementById("percentage-tip");
+const inputTipAmount = document.getElementById("tip-amount");
+const inputTotal = document.getElementById("total");
+const btnCalculate = document.getElementById("calculate");
+
+btnCalculate.addEventListener("click", ()=>{
+    inputTipAmount.value = inputBillAmount.value * (inputPercentageTip.value/100);
+    inputTotal.value = Number(inputBillAmount.value)+Number(inputTipAmount.value);
+});
